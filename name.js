860e4d81bd2fb688b1bf7f653ec36659c8695d19
Leonardo-Corazzini <leocora10@gmail.com>
2024-11-15 14:39:52 +0100

    ADD: username function

---
+++ name.js
@@ -0,0 +1,11 @@
+function userName (firstName,lastName){
+    const user = {
+        firstName,
+        lastName
+    }
+    return user  
+    }
+
+console.log(userName('leonardo','dhauid'))
+
+MediaSourceHandle.exports = userName
